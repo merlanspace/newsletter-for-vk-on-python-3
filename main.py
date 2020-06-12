a21a209eb6b9f0e5efd7db28c8001be7f4033444
Merlan <56320279+merlanspace@users.noreply.github.com>
2020-06-13 02:54:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 # -*- coding: utf-8 -*-
 import vk_api
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 vk_session = vk_api.VkApi(token='token')
 vk = vk_session.get_api()
-longpoll = VkBotLongPoll(vk_session, group_id)
 
-
-mailing = vk.messages.getConversations(count=200, filter="all", group_id="group_id")['items']
+mailing = vk.messages.getConversations(count=200, filter="all", group_id="group_id")['items'] #Можете добавить offset, если рассылка делается больше 200
 text = "text"
 for peer_id in mailing:
    peer = peer_id.get('conversation').get('peer').get('id')
-   print("id" + str(peer))
    try:
        vk.messages.send(
            peer_id=peer,
-           message=text
+           message=text,
            random_id=0
        )
-   except Exception:
-       pass
+   except BaseException as r:
+      print(r)
+      pass
 print('Newsletter is over!')
